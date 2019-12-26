8fe38d0cab23572ab9c71e304ff49323c6c79e16
Yasin Shamrat <yshamrat@gmail.com>
2019-12-26 11:16:19 +0600

    Initial Commit

---
+++ Mobile Info/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile Info
+//
+//  Created by Yasin Shamrat on 12/26/19.
+//  Copyright © 2019 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
